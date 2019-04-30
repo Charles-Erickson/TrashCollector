2e37784d24f4f4b36dd386127ddbd75fe48fab8a
Charles Erickson <cse621@gmail.com>
2019-04-30 13:12:35 -0500

    Built a model for customers

---
+++ TrashCollector/TrashCollector/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Billing { get; set; }
+        public int Zipcode { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+
+    }
+}
